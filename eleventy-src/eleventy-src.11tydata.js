b07ffff05a9f2bc383d318f36648de33027e04f3
Mohammad Mohammad <m91.alahmadi@gmail.com>
2020-06-07 21:42:56 -0400

    added basic data for index

--- eleventy-src/eleventy-src.11tydata.js
+++ eleventy-src/eleventy-src.11tydata.js
@@ -1,13 +1,43 @@
 const glob = require('glob');
 
+const jsFiles = glob.sync('**/*.js', { cwd: 'build' });
+const cssFiles = glob.sync('**/*.css', { cwd: 'build' });
+
+const shortAboutMe =`
+I'm Kuwaiti developer living in Brooklyn NY. My current tech interests are 
+c# (dotnet core) and cloud technologies (AWS). In the past I've done front development 
+with Angular and some React. My hobies include weightlifting, blogging, and Zumba. 
+`;
+
+const social = {
+    linkedin: {
+        url: "https://www.linkedin.com/in/mohammad91",
+        icon: "linkedin-in"
+    },
+    github: {
+        url: "https://github.com/AlahmadiQ8",
+        icon: "git"
+    },
+    email: {
+        url: "mailto:m91.alahmadi@gmail.com?Subject=[from my site]",
+        icon: "envelope"
+    },
+    stackoverflow: {
+        url: "https://stackoverflow.com/users/5431968/mmohammad",
+        icon: "stack-overflow"
+    }
+};
+
 module.exports = () =>{
 
-    const jsFiles = glob.sync('**/*.js', { cwd: 'build' });
-    const cssFiles = glob.sync('**/*.css', { cwd: 'build' });
-
     return {
+        title: "Mohammad's personal space",
+        description: `Mohammad's personals website`,
+        shortAboutMe,
+        Author: "Mohammad Mohammad",
         layout: 'layouts/base.njk',
         jsFiles,
-        cssFiles
+        cssFiles,
+        ...social
     };
 };
